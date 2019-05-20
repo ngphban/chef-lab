d0d961be6153555971f014e0d777cd5c8812012b
Nguyen Phuc Bao An <npban@yahoo.com>
2019-05-21 02:32:34 +0700

    Change Ubuntu release + chef-core-server version

--- provision/bootstrap-chef-server.sh
+++ provision/bootstrap-chef-server.sh
@@ -4,8 +4,8 @@ apt-get update -y -qq > /dev/null
 apt-get upgrade -y -qq > /dev/null
 apt-get -y -q install linux-headers-$(uname -r) build-essential > /dev/null
 
-wget -P /tmp https://packages.chef.io/stable/ubuntu/14.04/chef-server-core_12.4.1-1_amd64.deb > /dev/null
-dpkg -i /tmp/chef-server-core_12.4.1-1_amd64.deb
+wget -P /tmp https://packages.chef.io/files/current/chef-server/12.19.44/ubuntu/16.04/chef-server-core_12.19.44-1_amd64.deb > /dev/null
+dpkg -i /tmp/chef-server-core_12.19.44-1_amd64.deb
 
 chown -R vagrant:vagrant /home/vagrant
 
